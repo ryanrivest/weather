f8c309964c9afacce2f9be02b5d0170440cbd4db
Ryan Rivest <ryan.rivest@gmail.com>
2024-02-09 22:10:07 -0800

    style updates

--- app.vue
+++ app.vue
@@ -5,9 +5,9 @@ const isCelcius = ref(false);
 <template>
   <main class="gray-100 container mx-auto h-screen max-w-4xl p-5">
     <div
-      class="space-between justify-content flex w-full flex-wrap items-center gap-4 md:flex-nowrap md:justify-normal md:gap-8"
+      class="space-between justify-content flex w-full flex-wrap items-center gap-4 md:flex-nowrap md:justify-normal md:gap-0"
     >
-      <img src="/images/logo.svg" alt="Just Weather Logo" class="mx-auto h-7 w-full md:w-auto" />
+      <img src="/images/logo.svg" alt="Just Weather Logo" class="mx-auto h-7 w-full md:ml-auto md:mr-0 md:w-auto" />
       <div class="mx-auto flex w-full max-w-lg grow items-center gap-4">
         <Search class="grow" />
         <Toggle v-model="isCelcius" class="flex-shrink-0" />
